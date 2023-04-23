2805fda3e115d59301246a97dc6e991487ba9e97
Brian Iverson <brian.iverson@tuebora.com>
2023-04-23 14:55:12 -0500

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { SignInButton, SignOutButton, useUser } from '@clerk/nextjs';
 import { type NextPage } from "next";
 import Head from "next/head";
+import { api } from '~/utils/api';
 
 const Home: NextPage = () => {
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -16,6 +18,9 @@ const Home: NextPage = () => {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
         </div>
       </main>
     </>
