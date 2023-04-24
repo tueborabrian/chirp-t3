4959142b16c44be75f13eca631f737ae704c7105
Brian Iverson <brian.iverson@tuebora.com>
2023-04-24 14:51:15 -0500

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const PostView = (props: PostWithUser) => {
         <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span><span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
