d2c1e2425839b3e82fc34401bf40e577fc25fb03
Brian Iverson <brian.iverson@tuebora.com>
2023-04-25 10:58:50 -0500

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,10 @@ import { api } from '~/utils/api';
 import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { LoadingPage } from '~/components/loading';
+import { LoadingPage, LoadingSpinner } from '~/components/loading';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
+import Link from 'next/link';
 
 dayjs.extend(relativeTime);
 
@@ -22,6 +24,15 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
     }
   });
 
@@ -32,8 +43,16 @@ const CreatePostWizard = () => {
   return (
     <div className="flex w-full gap-3">
       <Image src={user.profileImageUrl} alt="Profile image" className="w-14 h-14 rounded-full" width={56} height={56} />
-      <input placeholder="Type some emojis!" className="grow bg-transparent outline-none" value={input} onChange={(e) => setInput(e.target.value)} disabled={isPosting} />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      <input placeholder="Type some emojis!" className="grow bg-transparent outline-none" value={input} onChange={(e) => setInput(e.target.value)} />
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })} disabled={isPosting}>Post</button>
+      )}
+
+      {isPosting && (
+        <div className="flex justify-center items-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   )
 };
@@ -47,9 +66,13 @@ const PostView = (props: PostWithUser) => {
       <Image src={author.profileImageUrl} alt={`@${author.username} profile image`} className="w-14 h-14 rounded-full" width={56} height={56} />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span><span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span><span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
-        <span className="text-2xl">{post.content}</span>
+        <Link href={`/post/${post.id}`}>
+          <span className="text-2xl">{post.content}</span>
+        </Link>
       </div>
     </div>
   );
@@ -89,7 +112,7 @@ const Home: NextPage = () => {
         <div className="h-full w-full md:max-w-2xl border-x border-slate-400">
           <div className="border-b border-slate-400 p-4">
             {!isSignedIn && (
-              <div className="flex justif-center">
+              <div className="flex justify-center">
                 <SignInButton />
               </div>
             )}
